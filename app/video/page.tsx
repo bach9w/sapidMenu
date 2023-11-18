cd55160211e70dec53387c5b796d8b74ae75b71e
bach9w <78446671+bach9w@users.noreply.github.com>
2023-11-18 16:42:54 +0200

    Update Livestream component to display "Monika"

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -4,7 +4,7 @@ import Livestream from './Livestream'; // Актуализирайте пътя 
 const VideoPage = () => {
 	return (
 		<div>
-			<Livestream />
+			<h1 className="text-center">Monika</h1>
 		</div>
 	);
 };
