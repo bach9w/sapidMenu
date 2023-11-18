1f0eb664e4ad2555ca853a5cbf309d4fa74fceaf
bach9w <78446671+bach9w@users.noreply.github.com>
2023-11-18 16:36:39 +0200

    Update Livestream component import and usage

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -1,34 +1,12 @@
 import React from 'react';
+import Livestream from './Livestream'; // Актуализирайте пътя според структурата на вашия проект
 
-// Определяне на типа за props на компонента
-type LivestreamProps = {
-	width?: number;
-	height?: number;
-	autoPlay?: boolean;
-	mute?: boolean;
-};
-
-// Дефиниране на компонента
-const Livestream: React.FC<LivestreamProps> = ({
-	width = 3824,
-	height = 2160,
-	autoPlay = true,
-	mute = true,
-}) => {
-	const src = `https://livestream.com/accounts/89344/events/8528180/player?width=${width}&height=${height}&enableInfoAndActivity=true&defaultDrawer=feed&autoPlay=${autoPlay}&mute=${mute}`;
-
+const VideoPage = () => {
 	return (
-		<iframe
-			id="ls_embed_1700317585"
-			src={src}
-			width={width}
-			height={height}
-			frameBorder="0"
-			scrolling="no"
-			allowFullScreen
-		></iframe>
+		<div>
+			<Livestream />
+		</div>
 	);
 };
 
-// Експортиране на компонента
-export default Livestream;
+export default VideoPage;
